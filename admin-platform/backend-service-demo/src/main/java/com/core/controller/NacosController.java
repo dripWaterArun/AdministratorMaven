f8a40adc2b9ad37bd00fa25fc7f8eb1bfa32fceb
15822623292 <daixu_cisco@sina.cn>
2021-06-15 22:12:43 +0800

    【V1.6】Nacos 读取多配置文件

--- admin-platform/backend-service-demo/src/main/java/com/core/controller/NacosController.java
+++ admin-platform/backend-service-demo/src/main/java/com/core/controller/NacosController.java
@@ -11,19 +11,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/nacos")
 public class NacosController {
 
-    @Value("${useLocalCache:false}")
-    private boolean useLocalCache;
+    @Value("${your.configuration}")
+    private String useLocalCache;
 
-    @Value("${user}")
-    private String user;
+//    @Value("${user}")
+//    private String user;
+
+    @Value("${config}")
+    private String config;
 
     @GetMapping("/get")
-    public boolean get() {
+    public String get() {
         return useLocalCache;
     }
 
-    @GetMapping("/get2")
-    public String get2() {
-        return user;
+//    @GetMapping("/get2")
+//    public String get2() {
+//        return user;
+//    }
+
+    @GetMapping("/get3")
+    public String get3() {
+        return config;
     }
 }
