c0ed4c3b59e605abed2686f7f476f2b5ce97be23
15822623292 <daixu_cisco@sina.cn>
2021-06-30 21:23:06 +0800

    【V 3.1】[修改] 连接数据库（完成）

--- admin-platform/backend-service-demo/src/main/java/com/core/platform/demo/po/BackendDemo.java
+++ admin-platform/backend-service-demo/src/main/java/com/core/platform/demo/po/BackendDemo.java
@@ -1,8 +1,10 @@
 package com.core.platform.demo.po;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName(value = "demo")
 public class BackendDemo {
 
     private Long id;
