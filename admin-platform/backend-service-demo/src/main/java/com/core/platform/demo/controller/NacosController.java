d5b5571fc16d1fb3d69315ebea858ca76323e8cc
15822623292 <daixu_cisco@sina.cn>
2021-07-01 22:18:38 +0800

    【V 3.2】[优化] 配置文件优化，可正常查询数据库

--- admin-platform/backend-service-demo/src/main/java/com/core/platform/demo/controller/NacosController.java
+++ admin-platform/backend-service-demo/src/main/java/com/core/platform/demo/controller/NacosController.java
@@ -1,19 +1,24 @@
 package com.core.platform.demo.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RefreshScope
-//@RestController
-//@RequestMapping("/nacos")
+@RefreshScope
+@RestController
+@RequestMapping("/nacos")
+@Api(tags = "案例模板:测试表（mybatis-plus 方式）")
 public class NacosController {
 
 //    @Value("${your.configuration}")
 //    private String useLocalCache;
 //
-//    @Value("${yours}")
-//    private String yours;
+    @Value("${configs.datasource.platform-demo.address}")
+    private String yours;
 //
 //    @Value("${config}")
 //    private String config;
@@ -23,10 +28,11 @@ public class NacosController {
 //        return useLocalCache;
 //    }
 //
-//    @GetMapping("/get2")
-//    public String get2() {
-//        return yours;
-//    }
+    @GetMapping("/get2")
+    @ApiOperation(value = "测试配置文件",notes="@author daixu @date 2021-06-28")
+    public String get2() {
+        return yours;
+    }
 //
 //    @GetMapping("/get3")
 //    public String get3() {
